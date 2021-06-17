2040186ebeb9a40c30a4232dae03f15617ca0526
Christian Escobar <ch.escobarf@Christians-MacBook-Pro.local>
2021-06-17 11:32:08 -0400

    Fix Patch Notification

--- Service-Component/node/middlewares/notificationPatch.ts
+++ Service-Component/node/middlewares/notificationPatch.ts
@@ -13,7 +13,9 @@ const http=axios.create({
     VtexIdclientAutCookie: key,
     "REST-Range": `resources=0-1`,
     "Cache-Control": "no-cache",
-    "X-Vtex-Use-Https": true
+    "X-Vtex-Use-Https": true,
+    "Content-Type": "application/json",
+    Accept: "application/json"
   }
 })
 
@@ -24,14 +26,14 @@ const searchID = await http.get(
 const idEntity=searchID.data[0].id;
 const estado = status.toString().replace("-","");
 
-const bodyJson = JSON.stringify({ [estado]:number });
-
 //Hacemos el Patch a MD para actualizar los el numero de notificaciones para el status
-const data = await http.patch(
-  `https://${ctx.vtex.account}.myvtex.com/api/dataentities/OM/documents/${idEntity}`,bodyJson
+const {data} = await http.patch(
+  `http://${ctx.vtex.account}.myvtex.com/api/dataentities/OM/documents/${idEntity}`,{[estado]:number}
 )
-
-const response=data
+console.log(data);
+const response={
+  [estado]:number
+}
 ctx.status=200
 ctx.body=response
 ctx.set('Cache-Control','no-cache')
